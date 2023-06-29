a51fe060b5417940bb6cf33704740790ff035416
Rein Velt <rein@velt.org>
2023-06-30 00:01:59 +0200

    poleposition

--- ledder/animations/Gamedemos/PolePosition.ts
+++ ledder/animations/Gamedemos/PolePosition.ts
@@ -11,23 +11,27 @@ import DrawAsciiArtColor from "../../draw/DrawAsciiArtColor.js"
 
 
 
-
-
-
-
 const car = `
-..5rrr5..
-..5rrr5..
+..rrrrr
+..rrrrr..
 .00rrr00.
-.55rrr55.
-.55rrr55.
+.00...00.
 `
+
 
 const sky =`
-....yyy..............55wwwww55................55ww55..........................55wwwwwwwwwwwww55.......
-...yyyyy............55wwwwww55..55wwww......55wwwwwwwwww55....................5wwwwwwwwwwwwwwwww5w5w..
-....yyy..............55wwwwwwwwwwwwww55.......55www55...55ww55..................5w5w5wwwwww55wwwwwwww.
+....www...................ww.................yyy.........................................................
+...wwwww.wwww...........wwwwww....wwwwww....yyyyy...wwww.....wwww..................www....ww....wwwww....
+...wwwwwwwwwww...........wwwwwwwwwwwwww.....yyyyy...wwwww...wwwwwww..............wwwwww.wwwww.wwwwwwww...
+....wwwwwwwwwwww..........wwwwwwwwwwwwwww....yyy.....wwwwwwwwwwwwwww...........wwwwwwwwwwwwwwwwwwwwww....
 `
+
+const horiz = `
+........g.........ggg..........o...9.oo....oo...............o...........5555.......b....oo.........o...
+......o.o.o...o....o.o.o....o.o...5.ooo...ooooo...oooo.oo...o.o.o.o.o.o.5555.r.g.t.r.o.ooooo.....o.o...
+`
+
+
 
 
 
@@ -45,14 +49,16 @@ export default class Planetsgone extends Animator {
         const intervalControl = controls.value("Animation interval", 1, 1, 10, 0.1)
         let bglist=new PixelList()
         let skylist=new PixelList();
+        let horizonlist=new PixelList();
         let carpixelList=new PixelList()
         let roadpixelList=new PixelList()
         box.add(bglist)
         box.add(skylist);
-        
+        box.add(horizonlist);
         box.add(roadpixelList)
         box.add(carpixelList)
         skylist.wrapX(box)
+        let horizonY=(box.height()/3)
     
     
         for (let i=0;i<box.height();i++)
@@ -60,12 +66,14 @@ export default class Planetsgone extends Animator {
           let color=new Color(0,0,255,1)
           for (let j=0; j<box.width();j++)
           {
-              if (i<4) { color=new Color(0,0,255,1); }
-              if (i>4) { color=new Color(0,255,0,1); }
+              if (i<horizonY) { color=new Color(0,0,255,1); }
+              if (i>horizonY) { color=new Color(0,255,0,1); }
               bglist.add(new Pixel(j,i,color))
           }
         }
       let time=0;
+
+     
 
        let road=[0,0,0,0,-1,-1,-1,0,0,0,0,0,1,2,3,2,3,3,2,3,4,4,4,4,5,5,5,5,6,7,8,9,10,11,12,13,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0,-1,-2,-3,-4,-5,-6,-7,-8,-9,-10,-11,-12,-13,-14,-15,-16,-17,-18,-17,-16,-15,-14,-13,-12,-11,-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,-0,-1,-1,-1,-1,-1,1,1,0,0,0,-1,1,-1,1,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
@@ -76,6 +84,7 @@ export default class Planetsgone extends Animator {
         carpixelList.clear()
         roadpixelList.clear()
         skylist.clear()
+        horizonlist.clear()
         time=time+speedControl.value;
       
         
@@ -83,38 +92,36 @@ export default class Planetsgone extends Animator {
         let centerY=box.yMax/2
 
         let width=box.xMax;
-        let halfwidth=width/2;
-        for (let i=0;i<box.yMax-4;i++)
+        let roadwidth=width/3;
+        for (let i=0;i<box.yMax-horizonY;i++)
         {
           //draw road
           let r=Math.round((frameNr+i)/10)%road.length
-            for (let k=centerX-halfwidth+road[r];k<centerX+halfwidth+road[r];k++)
+            for (let k=centerX-roadwidth+road[r];k<centerX+roadwidth+road[r];k++)
             {
-              roadpixelList.add(new Pixel(k,box.yMax-i,new Color(64,64,64,1)))
+              roadpixelList.add(new Pixel(k,box.yMax-i,new Color(100,100,100,1)))
             }
            
-            halfwidth=(halfwidth)*0.9
+            roadwidth=(roadwidth)*0.9
+            roadpixelList.add(new Pixel(centerX-roadwidth+road[r]-2,box.yMax-i,new Color(200,200,200,1))) //left side road striping
+            roadpixelList.add(new Pixel(centerX-roadwidth+road[r]-3,box.yMax-i,new Color(200,200,200,1))) //left side road striping
+            roadpixelList.add(new Pixel(centerX+roadwidth+road[r]+1,box.yMax-i,new Color(200,200,200,1))) //right side road striping
+            roadpixelList.add(new Pixel(centerX+roadwidth+road[r]+2,box.yMax-i,new Color(200,200,200,1))) //right side road striping
             if (((frameNr/10)+i)%4>2) 
             { 
               //draw road centerline
-              roadpixelList.add(new Pixel(centerX+road[r],box.yMax-i,new Color(128,128,128,1))) 
+              roadpixelList.add(new Pixel(centerX+road[r],box.yMax-i,new Color(200,200,200,1-(1/(i+1)))))
             }
-            else
-            {
-              //draw road sides
-              roadpixelList.add(new Pixel(centerX-halfwidth+road[r],box.yMax-i,new Color(128,128,128,1)))
-              roadpixelList.add(new Pixel(centerX+halfwidth+road[r],box.yMax-i,new Color(128,128,128,1)))
-              roadpixelList.add(new Pixel(centerX+road[r],box.yMax-i,new Color(0,0,0,1))) 
-            }
+           
         }
 
 
         let skyx=Math.round((frameNr)/10)%road.length
-        skylist.add(new DrawAsciiArtColor(road[skyx]*-1,0,  sky))
-
+        skylist.add(new DrawAsciiArtColor(Math.round(road[skyx]*-0.2-40),0,  sky))
+        horizonlist.add(new DrawAsciiArtColor(road[skyx]*-0.5,horizonY-1,  horiz))
         
         let carX=centerX-3
-        let carY=centerY+4;
+        let carY=box.height()-5;
         carpixelList.add(new DrawAsciiArtColor(carX+(road[skyx]/2),carY,  car))
 
 
